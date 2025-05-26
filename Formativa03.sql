95fd4e0475854ab30aaf29d910ab7d668976ad27
Alyson Pereira dos Santos <53673071831@>
2025-05-26 17:01:51 -0300

    Upload File 26/05

--- Formativa03.sql
+++ Formativa03.sql
@@ -1,6 +1,35 @@
-CREATE DATABASE Formativa03
+USE Fastbank;
 
-CREATE TABLE(Empretismo)
-	data_solicitacao VARCHAR,
-     valor_solicitado DOUBLE
-    analise VARCHAR
+-- Exercício 01 - Consulta de Análise
+
+/*
+SELECT 
+	dataSolicitacao AS 'Data de Solicitação',
+	CONCAT('R$ ', FORMAT(valorSolicitado, 2, 'pt_BR')) AS 'Valor Solicitado',
+CASE
+		WHEN analise = 1 THEN "Aprovado"
+		WHEN analise = 0 THEN "Negado"
+	ELSE "Indefinido"
+END AS Analise
+FROM Emprestimo;
+*/
+
+
+-- Exercício 02 - Consulta de Telefone
+
+SELECT 
+	numeros AS Números,
+	CASE
+			WHEN TRIM(observacoes) = '' OR observacoes IS NULL THEN 'Nada Consta'
+		ELSE observacoes
+	END AS Observações
+FROM Contato;
+
+    
+-- Exercício 03 - Número de Cartões
+
+SELECT *
+FROM cliente
+
+
+
